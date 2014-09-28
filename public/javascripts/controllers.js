ab61ef72b2ce934c164d83cd50988db9cb79ac2a
jesusblessf6 <jesusblessf6@gmail.com>
2014-09-28 16:09:14 +0800

    update

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -2,7 +2,7 @@
 
 var demoApp = angular.module('demoApp', []);
 
-demoApp.controller('phoneListContoller', function($scope){
+demoApp.controller('phoneListContoller', ['$scope', function($scope){
 	$scope.phones = [
 		{'name': 'Nexus S',
 		 'snippet': 'Fast just got faster with Nexus S.'},
@@ -11,10 +11,16 @@ demoApp.controller('phoneListContoller', function($scope){
 		{'name': 'MOTOROLA XOOM™',
 		 'snippet': 'The Next, Next Generation tablet.'}
 	];
-});
+}]);
 
-demoApp.controller('iPhoneListController', function($scope){
+demoApp.controller('iPhoneListController', ['$scope', function($scope){
 	$scope.iphones = [
 		'iphone2', 'iphone3', 'iphone3s', 'iphones4', 'iphone4s', 'iphone5', 'iphone5s', 'iphone5c', 'iphone6', 'iphone6+'
 	];
-});
+}]);
+
+demoApp.controller('remotePhonesController', ['$scope', '$http', function($scope, $http){
+	$http.get('/phones').success(function(data){
+		$scope.iphones = data;
+	});
+}]);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/phones', function(req, res){
+	res.json([
+		'iphone2', 'iphone3', 'iphone3s', 'iphones4', 'iphone4s', 'iphone5', 'iphone5s', 'iphone5c', 'iphone6', 'iphone6+'
+	]);
+});
+
 module.exports = router;
